f162bd5220d85158ce934677b8fad7f2c47bb763
SavinovaIren <irka887@gmail.com>
2022-07-12 23:52:10 +0300

    Работает проверка пароля

--- app.py
+++ app.py
@@ -24,19 +24,6 @@ def register_extensions(app):
     api.add_namespace(genre_ns)
     api.add_namespace(movie_ns)
     api.add_namespace(auth_ns)
-    create_data(app, db)
-
-def create_data(app, db):
-    with app.app_context():
-        db.create_all()
-
-        u1 = User(username="vasya", password=get_hash("my_little_pony"), role="user")
-        u2 = User(username="oleg", password=get_hash("qwerty"), role="user")
-        u3 = User(username="olga", password=get_hash("P@ssw0rd"), role="admin")
-
-        with db.session.begin():
-            db.session.add_all([u1, u2, u3])
-
 
 
 
