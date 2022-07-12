7b38945acca3133d46f7bcae77cecb97a3e5021d
SavinovaIren <103447690+SavinovaIren@users.noreply.github.com>
2022-07-13 01:03:43 +0300

    Update auth.py

--- views/auth.py
+++ views/auth.py
@@ -13,10 +13,49 @@ from setup_db import db
 auth_ns = Namespace("auth")
 
 
-
-
 def get_hash(password):
-    return hashlib.md5(password.encode('utf-8'))
+    return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+def generate_token(username,password, is_fresh=False):
+    if (password or username) is None:
+        abort(400)
+
+    user = db.session.query(User).filter(User.username == username).first()
+
+
+    if user is None:
+        return {"error": "Такого пользователя нет"}, 401
+
+    hash_hidest = get_hash(password)
+
+    if not is_fresh:
+       if user.password != hash_hidest:
+           return {"error": "Такого пароля нет"}, 401
+       else:
+           data = {"username": user.username,
+                "password": user.password}
+
+           min30 = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+           data["exp"] = calendar.timegm(min30.timetuple())
+           access_token = jwt.encode(data, SECRET, algorithm="HS256")
+
+           days130 = datetime.datetime.utcnow() + datetime.timedelta(days=130)
+           data["exp"] = calendar.timegm(days130.timetuple())
+           refresh_token = jwt.encode(data, SECRET, algorithm="HS256")
+
+       return {"access_token": access_token, "refresh_token": refresh_token}
+
+def approve_refresh_token(refresh_token):
+    data = jwt.decode(jwt=refresh_token, key=SECRET, algorithms=['HS256'])
+    username = data.get("username")
+    user = db.session.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    now = calendar.timegm(datetime.datetime.utcnow().timetuple())
+    expired = data['exp']
+    if now > expired:
+        return False
+    return generate_token(username, user.password, is_fresh=True)
 
 @auth_ns.route("/")
 class AuthView(Resource):
@@ -25,32 +64,18 @@ class AuthView(Resource):
         username = req_json.get("username", None)
         password = req_json.get("password", None)
 
-        if (password or username) is None:
-            abort(400)
-
-        user = User.query.filter(User.username == username).first()
-
-        if user is None:
-            return {"error": "Такого пользователя нет"}, 401
-
-        hash_hidest = get_hash(password)
-
-        if hash_hidest != user.password:
-            return {"error": "Пароль неверный"}, 401
-        else:
-            data = {"username": user.username,
-                "password": user.password}
-
-
-        min30 = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
-        data["exp"] = calendar.timegm(min30.timetuple())
-        access_token = jwt.encode(data, SECRET, algorithm="md5")
-
-        days130 = datetime.datetime.utcnow() + datetime.timedelta(days=130)
-        data["exp"] = calendar.timegm(days130.timetuple())
-        refresh_token = jwt.encode(data, SECRET, algorithm="md5")
-
-        tokens = {"access_token": access_token, "refresh_token": refresh_token}
-
+        tokens = generate_token(username,password)
         return tokens, 201
+
+    def put(self):
+        data = request.json
+        token = data.get("refresh_token")
+        if not token:
+            return "Не задан токен", 400
+
+        tokens = approve_refresh_token(token)
+        if tokens:
+            return tokens
+        else:
+            return "Ошибка в запросе", 400
 
